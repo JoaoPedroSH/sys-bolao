d55c86ea73ab04368ae8d4f2a06d8c5fbcbcf301
Pedro Henrique Pinheiro Silva <pedrohenriquevgtp@gmail.com>
2023-01-30 11:25:31 -0300

    Update FormCreateBolao.vue

--- src/views/pages/create-bolao/components/FormCreateBolao.vue
+++ src/views/pages/create-bolao/components/FormCreateBolao.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-form class="multi-col-validation">
+  <v-form class="multi-col-validation" @submit="createBolao" >
     <v-row>
       <v-col cols="12" md="4">
         <v-text-field v-model="bolao.nomeBolao" label="Nome do Bolão" outlined dense hide-details requered></v-text-field>
@@ -23,7 +23,16 @@
       </v-col>
 
       <v-col cols="12">
+<<<<<<< Updated upstream
         <v-checkbox v-model="bolao.bolaoPrivado" label="Privado" class="mt-0" hide-details></v-checkbox>
+=======
+        <v-checkbox
+          v-model="bolaoPrivate"
+          label="Privado"
+          class="mt-0"
+          hide-details
+        ></v-checkbox>
+>>>>>>> Stashed changes
       </v-col>
       <br />
       <v-col cols="12" md="6">
@@ -54,8 +63,22 @@
 
 
       <v-col cols="12">
+<<<<<<< Updated upstream
         <v-btn type="button" color="primary" @click.prevent="passwordVerify()"> Criar </v-btn>
         <v-btn type="reset" outlined class="mx-2" @click="resertForm()"> Redefinir </v-btn>
+=======
+        <v-btn  value="criar" color="primary" @click.prevent="passwordVerify()">
+          Criar
+        </v-btn>
+        <v-btn
+          type="reset"
+          outlined
+          class="mx-2"
+          @click="resertForm()"
+        >
+          Redefinir
+        </v-btn>
+>>>>>>> Stashed changes
       </v-col>
     </v-row>
   </v-form>
@@ -95,6 +118,7 @@ export default {
       this.createBolao()
     },
     createBolao() {
+<<<<<<< Updated upstream
       axios.post(process.env.VUE_APP_URL_API + '/boloes', this.bolao).then(response => {
         this.resertForm();
         this.$swal('Bolão Cadastrado!');
@@ -104,6 +128,21 @@ export default {
         this.$swal('Não foi possível Cadastrar!');
       })
     },
+=======
+      axios.post('http://localhost/ServeSysBolao/services/create_bolao.php', {
+        nameBolao: this.nameBolao,
+        qtdUsers: this.qtdUsers,
+        description: this.description,
+        password: this.password,
+        confirmPassword: this.confirmPassword,
+        bolaoPrivate: this.bolaoPrivate
+      }).then((response) => {
+        console.log(response);
+      })
+      e.preventeDefault();
+    },
+    
+>>>>>>> Stashed changes
   },
 }
 </script>
